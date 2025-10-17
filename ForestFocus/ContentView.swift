6c05a0b4a0d2476de2f06882c65a506711f86d83
WilliamS <73660888+cnwyshi@users.noreply.github.com>
2025-10-16 21:42:59 -0700

    Initial Commit

---
+++ ForestFocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForestFocus
+//
+//  Created by flame on 10/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForestFocus/ForestFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestFocusApp.swift
+//  ForestFocus
+//
+//  Created by flame on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ForestFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
